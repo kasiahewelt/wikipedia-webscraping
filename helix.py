7c773fd0f5071550bbb0bb562d6b60fb13bc803c
Maciej Kuźma <m.kuzma@rynekpierwotny.pl>
2022-10-11 20:58:34 +0200

    Small improvements

--- helix.py
+++ helix.py
@@ -4,12 +4,18 @@ from wikipedia import PageError
 
 df = pd.read_csv("helix_no_description.csv")
 
-print(df.T)
+val = []
 companies_list = df['self_firmo_name___'].values.tolist()
-val = [wikipedia.summary("%s" % company, auto_suggest=False, redirect=True) for company in companies_list]
+for company in companies_list:
+    try:
+        print(wikipedia.summary("%s" % company, auto_suggest=False, redirect=True))
+        val.append(wikipedia.summary("%s" % company, auto_suggest=False, redirect=True))
+    except PageError as e:
+        print(e)
 
 
 def form_df():
+    print(df.T)
     try:
         dicts = {}
         for k, v in zip(companies_list, val):
